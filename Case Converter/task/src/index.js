3bf011dd9a44c1da0f19a7cce40c0d44ab53058a
Anastasia_Bachina <bachina2192@gmail.com>
2022-04-16 19:02:15 +0300

    Sentence Case

--- Case Converter/task/src/index.js
+++ Case Converter/task/src/index.js
@@ -2,25 +2,39 @@ function UpperCase() {
     let up = document.querySelector('textarea').value.toUpperCase().split(' ');
     document.querySelector('textarea').value = up.join(' ');
 }
+
 document.getElementById("upper-case").addEventListener("click", UpperCase);
 
 function LowerCase() {
     let low = document.querySelector('textarea').value.toLowerCase().split(' ');
     document.querySelector('textarea').value = low.join(' ');
 }
+
 document.getElementById("lower-case").addEventListener("click", LowerCase);
 
 function ProperCase() {
     let prop = document.querySelector('textarea').value.split(' ');//обращается к ид окошка
     for (let i = 0; i < prop.length; i++) {
-        prop[i] = prop[i][0].toUpperCase() + prop[i].substring(1,prop[i].length)
+        prop[i] = prop[i][0].toUpperCase() + prop[i].substring(1, prop[i].length)
     }
     document.querySelector('textarea').value = prop.join(' ');//выводит значение в окошко
-
 }
+
 document.getElementById("upper-case").addEventListener("click", ProperCase)//норм вызов
 
-function SentenceCase(){
-
+function SentenceCase() {
+    let prop = document.querySelector('textarea').value // получаем текст
+    let prop2 = ""
+    let isLastDot = true
+    for (let i = 0; i < prop.length - 1; i++) {
+        if (isLastDot === true) {
+            prop2 = prop2 + prop[i].toUpperCase()
+        } else {
+            prop2 = prop2 + prop[i].toLowerCase()
+        }
+        isLastDot = prop[i - 1] === '.' && prop[i] === ' '; //'. ab'
+    }
+    document.querySelector('textarea').value = prop2;//выводит значение в окошко
 }
+
 document.getElementById("upper-case").addEventListener("click", SentenceCase)
