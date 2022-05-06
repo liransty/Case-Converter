fc59e6ca72e5c293309f9eabda74f9261c2db29d
Anastasia_Bachina <bachina2192@gmail.com>
2022-05-06 23:43:51 +0300

    Save

--- Case Converter/task/src/index.js
+++ Case Converter/task/src/index.js
@@ -39,3 +39,23 @@ function SentenceCase() {
 
 document.getElementById("upper-case").addEventListener("click", SentenceCase)
 
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+function download1() {
+    let text = document.getElementById("text").value;
+    let filename = "text.txt"
+    download(filename, text);
+}
+
+document.getElementById("save-text-file").addEventListener("click", download1, false);
